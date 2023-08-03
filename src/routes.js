19fca5eba7629c142387d51d7908be00f5345651
Marco Antonio Lira Barros <malirab2302@gmail.com>
2023-08-03 16:21:29 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const routes = require('express').Router()
+const multer = require('multer')
+const multerConfig = require('./config/multer')
+
+const Post = require('./models/Post')
+
+routes.post('/posts', multer(multerConfig).single('file'), async (req, res)=>{
+
+    const post = await Post.create({
+        name: req.file.originalname,
+        size: req.file.size,
+        key: req.file.filename,
+        url: '',
+    })
+
+    return res.json({ 'name': 'Marco Antonio'})
+})
+
+module.exports = routes
